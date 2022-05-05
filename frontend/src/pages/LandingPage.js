19874d00c10aa1f9fcb663390ce44b27536bb019
Jeffrey Rosenberg <jrosenberg@gelaw.com>
2022-05-05 08:14:27 -0400

    comments

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -5,7 +5,6 @@ import TestSlider from './ArtifactSlider';
 import 'antd/dist/antd.css';
 import './landingpage.css';
 import HypoSlider from './HypoSlider';
-import MethodMaker from './MethodMaker';
 import { Layout, Menu, Breadcrumb, Button } from 'antd';
 //This landing page is acutally the Method Maker page, it relied on the DataSlider (TestSlider) and HypoSlider along with their global variables to submit methods
 // A mehtod is when a user borrows and/or makes artifcats, methods should have titles
